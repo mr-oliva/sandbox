5b2b56510ebb07c1ed3a20abedc4e2eb4d43804e
bookun <kutsuzawa2851@gmail.com>
2019-07-02 22:58:06 +0900

    print消した

--- go/judge-clientip/driver/cache/firebase.go
+++ go/judge-clientip/driver/cache/firebase.go
@@ -2,7 +2,6 @@ package cache
 
 import (
 	"context"
-	"fmt"
 
 	"cloud.google.com/go/firestore"
 	firebase "firebase.google.com/go"
@@ -35,21 +34,16 @@ func (f *Firebase) Add(ctx context.Context, ip string, result entity.Result) err
 func (f *Firebase) Get(ctx context.Context, ip string) (entity.Result, error) {
 	result := entity.Result{}
 	cache, err := f.client.Collection("ocn-ip").Doc(ip).Get(ctx)
-	fmt.Println("add cache1")
-	println("add cache1")
 	if !cache.Exists() {
 		return result, nil
 	}
-	fmt.Println("add cache2")
 	if err != nil {
 		return result, err
 	}
 	//if err := cache.DataTo(&result); err != nil {
 	//	return result, err
 	//}
-	fmt.Println("add cache3")
 	cache.DataTo(&result)
-	fmt.Println("add cache4")
 	result.From = "cache"
 	return result, nil
 }
