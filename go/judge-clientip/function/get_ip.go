26d296ae1f7a46a4fe723f5ad976f5a231e8b0a5
bookun <kutsuzawa2851@gmail.com>
2019-07-03 15:23:29 +0900

    add logging

--- go/judge-clientip/function/get_ip.go
+++ go/judge-clientip/function/get_ip.go
@@ -78,6 +78,7 @@ func GetIP(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
+		log.Printf("ip: %s\thost: %s\tkind: %s\tservice: %s\n", cacheResult.IP, cacheResult.Host, cacheResult.Kind, service)
 		fmt.Fprint(w, resultBuf.String())
 		return
 	}
@@ -95,6 +96,7 @@ func GetIP(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		fmt.Fprint(w, resultBuf.String())
+		log.Printf("ip: %s\thost: %s\tkind: %s\tservice: %s\n", clientIP, "-", "no_set", service)
 		return
 	}
 
@@ -110,6 +112,7 @@ func GetIP(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+	log.Printf("ip: %s\thost: %s\tkind: %s\tservice: %s\n", clientIP, host, kind, service)
 	fmt.Fprint(w, resultBuf.String())
 }
 
